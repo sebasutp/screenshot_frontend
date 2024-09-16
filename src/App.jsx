89a6f46b67ac36457aeb1266fe17a57d1f2672ef
Lucia MQ <103491565+luquiceno@users.noreply.github.com>
2024-09-16 17:54:55 +0200

    UI continues to be improved. Correcting the last commit to this file.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { Navigate, useNavigate } from 'react-router-dom';
 import './App.css'
-import Header from './components/Header.jsx';
 import Login from './components/Login.jsx'
 import Item from './components/Item.jsx';
 import Screenshots from './components/Screenshots.jsx';
@@ -13,43 +12,16 @@ import NavMenu from './components/NavMenu.jsx';
 function App() {
   //const [token, setToken] = useState(localStorage.getItem('token'))
   const token = localStorage.getItem('token')
-<<<<<<< Updated upstream
-  const navigate = useNavigate()
-
-  const handleLogout = () => {
-    //setToken(null)
-    localStorage.removeItem('token');
-    navigate('/login');
-    // Optionally redirect to login page or handle other logout logic
-  };
-
+ 
   return (
     <div>
-      {token ? (
-        <div name="logged_in_header">
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-        ) : (
-          <div name = "unknown_user_header"></div>
-        )
-      }
-      {/* <Header /> */}
-=======
-  
-  return (
-    <div>
->>>>>>> Stashed changes
       <Routes>
         {/* Public Route (accessible without login) */}
         <Route path="/login" element={<Login />} />
 
         {/* Protected Route (requires login) */}
-<<<<<<< Updated upstream
-        <Route path="/" element={token ? <Screenshots /> : <Navigate replace to="/login" />} />
-=======
         <Route path="/" element={token ? <NavMenu /> && <Screenshots /> : <Navigate replace to="/login" />} />
         {/* <Route path="/screenshots/" element={<Screenshots />} />  */}
->>>>>>> Stashed changes
         <Route path="/item/:id" element={<Item />} />
         <Route path="/screenshot/new" element={<NewScreenshot />} />
         <Route path="/screenshot/plugin" element={<NewScreenshotPlugin />} />
