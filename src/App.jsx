78a494c09c36286a2886923e08e77e4da1149be7
Lucia MQ <103491565+luquiceno@users.noreply.github.com>
2024-09-16 17:45:18 +0200

    UI continues to be improved.

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ import Item from './components/Item.jsx';
 import Screenshots from './components/Screenshots.jsx';
 import NewScreenshot from './components/NewScreenshot.jsx';
 import NewScreenshotPlugin from './components/PluginScreenshot.jsx';
+import NavMenu from './components/NavMenu.jsx';
 
 function App() {
   //const [token, setToken] = useState(localStorage.getItem('token'))
   const token = localStorage.getItem('token')
+<<<<<<< Updated upstream
   const navigate = useNavigate()
 
   const handleLogout = () => {
@@ -32,12 +34,22 @@ function App() {
         )
       }
       {/* <Header /> */}
+=======
+  
+  return (
+    <div>
+>>>>>>> Stashed changes
       <Routes>
         {/* Public Route (accessible without login) */}
         <Route path="/login" element={<Login />} />
 
         {/* Protected Route (requires login) */}
+<<<<<<< Updated upstream
         <Route path="/" element={token ? <Screenshots /> : <Navigate replace to="/login" />} />
+=======
+        <Route path="/" element={token ? <NavMenu /> && <Screenshots /> : <Navigate replace to="/login" />} />
+        {/* <Route path="/screenshots/" element={<Screenshots />} />  */}
+>>>>>>> Stashed changes
         <Route path="/item/:id" element={<Item />} />
         <Route path="/screenshot/new" element={<NewScreenshot />} />
         <Route path="/screenshot/plugin" element={<NewScreenshotPlugin />} />
