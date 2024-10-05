7a2f7733e6ad83245d9928aa0531a2ded723af7e
Sebastian Gomez-Gonzalez <sebastian@robot-learning.de>
2024-10-05 21:40:22 +0200

    Making the app compatible with relative paths

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -64,7 +64,7 @@ function NavMenu() {
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a 
-                  href="/" 
+                  href="./" 
                   className="block py-2 px-3 text-white rounded md:bg-transparent md:text-indigo-700 md:p-0 md:dark:text-indigo-500" 
                   aria-current="page"
                   >Home
